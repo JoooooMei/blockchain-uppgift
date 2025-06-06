6873a80638436ab0cadb4592d60f3a32a06b4fb7
JoooooMei <c.johan.meisel@gmail.com>
2025-06-06 11:19:18 +0200

    Forgot to commit this file

--- src/app.mjs
+++ src/app.mjs
@@ -1,8 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import errorHandeler from './middleware/errorHandler.mjs';
+import blockchainRouter from './routes/blockchain-routes.mjs';
+import { logger } from './middleware/logger.mjs';
 
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger);
+}
+
+app.use(express.json());
+app.use('/api/v1/blocks', blockchainRouter);
+app.use(errorHandeler);
 
 export { app };
